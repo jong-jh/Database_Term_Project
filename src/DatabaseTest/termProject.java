60a6f97710dfbe89dfac142e539f2dbd26663324
jong_jh <rhfksl0115@gamil.com>
2022-12-04 04:17:04 +0900

    menu 5,6,7 추가

--- src/DatabaseTest/termProject.java
+++ src/DatabaseTest/termProject.java
@@ -1,18 +1,23 @@
 package DatabaseTest;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class termProject {
 	
 	public static void printMenu() {
 		System.out.println();
-		System.out.println("------------------------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.println("1. connection                  2. show all members' Info");
 		System.out.println("3. insert Member               4. delete Member   ");
-		System.out.println("5. connection                  6. find ****      ");
+		System.out.println("5. account balance check       6. show each member's song");
+		System.out.println("7. insert Song                 8.   ");
 		System.out.println("                              99. quit");
-		System.out.println("------------------------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.printf("번호를 입력하세요 : ");
 	}
 
@@ -43,6 +48,7 @@ public class termProject {
 		while (key) {
 			printMenu();
 			num = sc.nextInt();
+			sc.nextLine();
 			switch (num) {
 			case 1: // connection
 				try {
@@ -91,6 +97,19 @@ public class termProject {
 				} catch (Exception e) {
 					System.out.println(e);
 				}
+				
+				System.out.println("팀을 지정해주세요");
+				String tName, position;
+				System.out.printf("팀이름을 입력하세요: ");
+				tName = sc.nextLine();
+				System.out.printf("맡은 포지션을 입력하세요: ");
+				position = sc.nextLine();
+				try {
+					stmt.executeUpdate("INSERT INTO BelongTo VALUES("+id+",\'"+tName+"\',\'"+position+"\')");
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				
 				break;
 			case 4:
 				System.out.printf("삭제할 id를 입력하세요: ");
@@ -106,10 +125,50 @@ public class termProject {
 				break;
 
 			case 5:
+				try {
+					ResultSet rs=stmt.executeQuery("select ( select sum(amount) from Account where type = \'deposit\' ) - (select sum(amount) from Account where type = \'withdraw\')");
+					System.out.printf("잔액 : ");
+					while(rs.next())
+						System.out.println(rs.getInt(1));
+				} catch (Exception e) {
+					System.out.println(e);
+				}
 				break;
 
 			case 6:
 
+				break;
+				
+			case 7:
+				System.out.println("노래 추가하기");
+				String singer, title, genre;
+				System.out.printf("가수를 입력하세요: ");
+				singer = sc.nextLine();
+				System.out.printf("곡 제목을 입력하세요: ");
+				title = sc.nextLine();
+				System.out.printf("장르를 입력하세요: ");
+				genre = sc.nextLine();
+				System.out.printf("담당 팀을 입력하세요: ");
+				tName = sc.nextLine();
+				try {
+					stmt.executeUpdate("INSERT INTO Song VALUES(\'"+singer+"\',\'"+title+"\',\'"+genre+"\',\'"+tName+"\')");
+					
+					ResultSet rs=stmt.executeQuery("SELECT mid FROM BelongTo where tname = "+ "\'"+tName+"\'");
+					
+					ArrayList<Integer> ids = new ArrayList();
+					while(rs.next()) {
+//						System.out.println(rs.getInt(1));
+//						stmt.executeUpdate("INSERT INTO Play VALUES("+rs.getInt(1)+",\'"+singer+"\',\'"+title+"\')");
+						ids.add(rs.getInt(1));
+						}
+					for(int i=0;i<ids.size();i++) {
+						stmt.executeUpdate("INSERT INTO Play VALUES("+ids.get(i)+",\'"+singer+"\',\'"+title+"\')");
+					}
+					
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				
 				break;
 
 			case 99:
